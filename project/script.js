ecd6f7a69ba20717beeaf13b6cb45b82c0e0b5b7
Clemens Krenglmueller <c.krenglmueller@students.htl-leonding.ac.at>
2025-03-18 16:51:34 +0100

    Pushed new Prototype (Figma)

--- project/script.js
+++ project/script.js
@@ -1,13 +1,53 @@
-fetch('/data/songData.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-    })
-  .then(songData => {
-    console.log('Song Data:', songData);
-  })
-  .catch(error => {
-    console.error('Error fetching song data:', error);
-  });
+const songData = [
+{
+            "id": 0,
+            "title": "Lighthouse (feat. PhiloSofie)",
+            "cover": "/songImage/lighthouse_JJD.jpg",
+            "artist": "JJD",
+            "releaseDate": 13032025,
+            "recent": true
+        },
+	    {
+            "id": 1,
+            "title": "Live Your Live",
+            "cover": "liveYourLive_Tobu.jpg",
+            "artist": "Tobu",
+            "releaseDate": 11032025,
+            "recent": true
+	    },
+        {
+            "id": 2,
+            "title": "Love Letter",
+            "cover": "liveYourLive_Tobu.jpg",
+            "artist": "m3gatron",
+            "releaseDate": 11032025,
+            "recent": true
+	    },
+        {
+            "id": 3,
+            "title": "Live Your Live",
+            "cover": "liveYourLive_Tobu.jpg",
+            "artist": "Tobu",
+            "releaseDate": 11032025,
+            "recent": false
+	    }
+]
+
+displaySongData(songData);
+
+function displaySongData(data) {
+    const newReleaseBoxes = document.getElementById('newReleaseBoxes');
+    newReleaseBoxes.innerHTML = '';
+
+    data.forEach(song => {
+        const songElement = document.createElement('div');
+        songElement.classList.add('song');
+        songElement.innerHTML = `
+            <img src="${song.cover}" alt="${song.title} cover">
+            <h3>${song.title}</h3>
+            <p>Artist: ${song.artist}</p>
+            <p>Release Date: ${new Date(song.releaseDate).toLocaleDateString()}</p>
+        `;
+        newReleaseBoxes.appendChild(songElement);
+    });
+}
