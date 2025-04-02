531de514ba4d92c290788420521cfedb5a6cbaf1
Clemens Krenglmueller <c.krenglmueller@students.htl-leonding.ac.at>
2025-04-02 13:48:23 +0200

    added artist and song data

--- project/data/artistData.js
+++ project/data/artistData.js
@@ -2,6 +2,16 @@ const artistData = [
     {
         "id":0,
         "name":"Tobu",
-        "cover":"data/artistImages/"
-    }
+        "cover":"data/artistImages/tobu.jpg"
+    },
+    {
+        "id":1,
+        "name":"Tobu",
+        "cover":"data/artistImages/tobu.jpg"
+    },
+    {
+        "id":2,
+        "name":"Tobu",
+        "cover":"data/artistImages/tobu.jpg"
+    },
 ]
--- project/data/songData.js
+++ project/data/songData.js
@@ -6,8 +6,9 @@ const songData = [
                 "artist": "JJD feat. PhiloSofie",
                 "releaseDate": "13-03-2025",
                 "recent": true,
-                "mp3":"data/",
-                "liked":false
+                "mp3":"data/songFiles/lighthouse.mp3",
+                "liked":false,
+                "genres":["future bass", "dramatic", "energetic", "powerful"]
             },
             {
                 "id": 1,
@@ -16,27 +17,30 @@ const songData = [
                 "artist": "Tobu",
                 "releaseDate": "11-03-2025",
                 "recent": true,
-                "mp3":"data/",
-                "liked":true
+                "mp3":"data/songFiles/liveYourLife.mp3",
+                "liked":true,
+                "genres":["futurepop", "energetic", "exciting", "positive"]
             },
             {
                 "id": 2,
                 "title": "Love Letter",
-                "cover": "data/songImages/liveYourLive_Tobu.jpg",
+                "cover": "data/songImages/loveLetter.jpg",
                 "artist": "m3gatron",
                 "releaseDate": "11-03-2025",
                 "recent": true,
-                "mp3":"data/",
-                "liked":true
+                "mp3":"data/songFiles/loveLetter.mp3",
+                "liked":true,
+                "genres":["electronic pop", "energetic", "exciting", "positive"]
             },
             {
                 "id": 3,
-                "title": "Live Your Live",
-                "cover": "data/songImages/liveYourLive_Tobu.jpg",
-                "artist": "Tobu",
-                "releaseDate": "11-03-2025",
+                "title": "Mortals Funk Remix",
+                "cover": "data/songImages/mortalsFunkRemix.jpg",
+                "artist": "Warriyo (remixed by LXNGVX)",
+                "releaseDate": "01-03-2025",
                 "recent": false,
-                "mp3":"data/",
-                "liked":false
+                "mp3":"data/songFiles/mortalsFunkRemix.mp3",
+                "liked":false,
+                "genres":["brazilian phonk", "powerful", "angry", "neutral"]
             }
     ]
